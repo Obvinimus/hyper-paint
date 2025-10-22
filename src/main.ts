976432bb9424a78efd01000fa18c23236cf83da5
Obvinimus <obvinimus@protonmail.com>
2025-10-22 17:58:31 +0200

    Basic HelloWorld! app

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-  </div>
-`
+let graphics: CanvasRenderingContext2D | null;
+let canvas: HTMLCanvasElement | null;
 
+
+
+function init() {
+  canvas = document.getElementById('maincanvas') as HTMLCanvasElement;
+  graphics = canvas.getContext('2d');
+  draw();
+}
+
+function draw(){
+  graphics?.fillText("Hello World!", 10, 50);
+}
+
+window.onload = init;
